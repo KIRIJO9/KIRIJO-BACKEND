84bc5e5862577ba634471855c516df578cf2a408
Pritam Barman <pritambarmanofficial1608@gmail.com>
2025-08-30 11:15:35 +0530

    file structure created

---
+++ src/routes/auth.router.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const router = express.Router();
+
+//route for login
+router.post("/login", (req, res) =>{
+    res.send("lOGIN")
+})
+
+//route for logout
+
+//route for signup
+
+//route for signup
+
+
+
+export default router

df020c00448850cdacf6ac10a171abd0cd419628
Aminur12-tech <alexdyel1234@gmail.com>
2025-08-30 23:15:02 +0530

    create the signup feature

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,12 +1,11 @@
 import express from "express";
-import { emailOtpVerification, emailVerification } from "../controllers/auth.controller.js";
+import { emailOtpVerification, emailVerification, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
-
 
 //route for email verification and otp
 router.post("/email-verification-otp-sending", emailVerification)
 router.post("/email-otp-verification", emailOtpVerification)
-
+router.post("/signup", signup);
 
 export default router
